0bd6b39129404cc8a8a44cd969210d8aa44505a8
abhay12-create <babajack880@gmail.com>
2026-06-24 17:50:33 +0530

    Create diamond-pattern.cpp

---
+++ diamond-pattern.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,nd,i;
+    cin>>n;
+    nd=(n+1)/2;
+    for(int r=1; r<=nd; r++){
+        for(i=1; i<=nd-r; i++){
+            cout<< " ";
+        }
+        for(i=1; i<=2*r-1;i++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for(int r=nd-1; r>=1; r--){
+        for(i=1; i<=nd-r; i++){
+            cout<< " ";
+        }
+        for(i=1; i<=2*r-1;i++){
+            cout<<"*";
+        }
+        cout<<endl;
+}
+return 0;
+}
